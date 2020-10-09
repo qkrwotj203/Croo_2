f130ef8af82fe56dbea09d5e3a52cd2bdd28718c
Bangnany <qkrwotj203@gmail.com>
2020-10-09 15:11:17 +0900

    refactoring

--- model/user.js
+++ model/user.js
@@ -1,5 +1,8 @@
 
 const mongoose = require('mongoose')
+const gravatar = require('gravatar')
+const normalize = require('normalize-url')
+const bcrypt = require('bcryptjs')
 
 const userSchema = mongoose.Schema({
 
@@ -22,12 +25,51 @@ const userSchema = mongoose.Schema({
         required: true
     },
     avatar: {
-        type: String,
-        required: true
+        type: String
+    }
+
+},
+    {
+        timestamps: true
+    }
+)
+
+userSchema.pre('save', async function (next) {
+
+    try {
+        console.log('entered')
+
+        //make a avatart
+        const avatar = await normalize(
+            gravatar.url(this.email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            }),
+            { forceHttps: true }
+        )
+        this.avatar = avatar
+
+        // encoding passowrd
+        const salt = await bcrypt.genSalt(10)
+        const passwordHash = await bcrypt.hash(this.password, salt)
+        this.password = passwordHash
+
+        console.log('exited')
+        next()
+    }
+
+    catch (error) {
+        next(error)
     }
 
 })
 
-
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err)
+        cb (null, isMatch)
+    })
+}
 
 module.exports = mongoose.model('user', userSchema)
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,17 @@
 
 const express = require('express')
 const router = express.Router()
-const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const gravatar = require('gravatar')
-const normalize = require('normalize-url')
 
 const userModel = require('../model/user')
 
 router.post('/register', (req, res) => {
 // 이메일 체크 - 비밀번호 암호화 - 저장하고
     const {nickName, email, phoneNumber, password} = req.body
+
+    // @route POST http://localhost:1818/user/register
+    // @desc register user
+    // @access public
 
     userModel
         .findOne({email})
@@ -21,46 +22,24 @@ router.post('/register', (req, res) => {
                 })
             }
             else {
-                bcrypt.hash(password, 10, (err, hash) => {
-                    if (err) {
-                        return res.json({
+
+                const newUser = new userModel({
+                    email, nickName, password, phoneNumber
+                })
+
+                newUser
+                    .save()
+                    .then(user => {
+                        res.json({
+                            message: 'WELCOME!!',
+                            userInfo: user
+                        })
+                    })
+                    .catch(err => {
+                        res.json({
                             message: err.message
                         })
-                    }
-                    else {
-                        //automation regist avatar
-                        const avatar = normalize(
-                            gravatar.url(email, {
-                                s: '200',
-                                r: 'pg',
-                                d: 'mm'
-                            }),
-                            { forceHttps: true }
-                        )
-
-                        const newUser = new userModel({
-                            nickName,
-                            email,
-                            phoneNumber,
-                            password,
-                            avatar
-                        })
-
-                        newUser
-                            .save()
-                            .then(user => {
-                                res.json({
-                                    message: 'WELCOME!!',
-                                    userInfo: user
-                                })
-                            })
-                            .catch(err => {
-                                res.json({
-                                    message: err.message
-                                })
-                            })
-                    }
-                })
+                    })
             }
         })
         .catch(err => {
@@ -68,19 +47,15 @@ router.post('/register', (req, res) => {
                 message: err.message
             })
         })
-
-
-
-
-
-
-
-})
+    })
 
 router.post('/login', (req, res) => {
     //이메일 매칭 - 패스워드 매칭 - 토큰 저장
     const {email, password} = req.body
 
+    // @route POST http://localhost:1818/user/login
+    // @desc login user
+    // @access privvate
     userModel
         .findOne({email})
         .then(user => {
@@ -88,34 +63,36 @@ router.post('/login', (req, res) => {
                 return res.json({
                     message: 'email doesnt exist'
                 })
-            }
-            else {
-                bcrypt.compare(password, user.password, (err, result) => {
-                    if (err || result === false) {
-                        res.json({
-                            message: 'wrong password'
-                        })
-                    }
-                    else {
-                        // 토큰 생성
-                        const token = jwt.sign(
-                            {
-                                email: user.email,
-                                id: user._id
-                            },
-                            'key',
-                            {expiresIn: '1d'}
-                        )
-                        //token return
-                        res.json({
-                            message:'auth successful',
-                            token: token
-                        })
+            } else {
+                user
+                    .comparePassword(password, (err, isMatch) => {
+                        if (err || !isMatch) {
+                            return res.json({
+                                message: 'wrong password'
+                            })
+                        }
 
-                    }
-                })
-            }
-        })
+
+                    else
+                        {
+                            // 토큰 생성
+                            const token = jwt.sign(
+                                {
+                                    email: user.email,
+                                    id: user._id
+                                },
+                                'key',
+                                {expiresIn: '1d'}
+                            )
+                            //token return
+                            res.json({
+                                message: 'auth successful',
+                                token: token
+                            })
+                        }
+                    })
+                 }
+            })
         .catch(err => {
             res.json({
                 message: err.message
